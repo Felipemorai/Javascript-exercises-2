f163dc6a8d896cc988930340749edfd94716b1b5
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-03 16:54:07 -0300

    list

---
+++ List.js
@@ -0,0 +1,11 @@
+const names = ['Apollo', 'Arthemis', 'Kiara', 'Peter'];
+
+const arthemis = names[1];
+
+console.log(arthemis);
+
+names.push(false);/* Add in the end */
+names.unshift('Frank');/* Add  */
+names.pop()/* Cut the last */
+
+console.log(names);
