07dffebd13f6d3e9abf2e70be003606a6a27d964
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-02 21:51:15 -0300

    strings

--- Variables.js
+++ Variables.js
@@ -1,1 +1,7 @@
-alert('Hello everyone!')
+/* Strings */
+
+const firstName = 'Felipe';
+const lastName = 'Morais';
+
+console.log('I am' + ' ' + firstName + ' ' + lastName);
+console.log(`I am ${firstName} ${lastName}`);
