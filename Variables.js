f46072ac67da81a2abf2f16848598898fb7d03e5
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-03 09:14:20 -0300

    booleans

--- Variables.js
+++ Variables.js
@@ -9,3 +9,24 @@ console.log('I am' + ' ' + firstName + ' ' + lastName);
 console.log(`I am ${firstName.toUpperCase()} ${lastName.toLowerCase()}`);
 
 console.log(names.split(','));
+
+/* Numbers */
+
+const number = 5;
+
+console.log(number.toString());
+console.log(typeof number.toString());
+
+/* Boolean */
+
+false
+true
+
+console.log(2 == '2');
+console.log(2 === '2');
+
+/* Null and Undefined */
+
+const x = null; /* Empty */
+
+const y = undefined; /* Still undefined */
