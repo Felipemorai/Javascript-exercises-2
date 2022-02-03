7e920cdde51bb4c1b475d7d791103dc1eda9f2b0
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-02 21:58:49 -0300

    split

--- Variables.js
+++ Variables.js
@@ -3,5 +3,9 @@
 const firstName = 'Felipe';
 const lastName = 'Morais';
 
+const names = 'Apollo, Arthemis, Kiara';
+
 console.log('I am' + ' ' + firstName + ' ' + lastName);
-console.log(`I am ${firstName} ${lastName}`);
+console.log(`I am ${firstName.toUpperCase()} ${lastName.toLowerCase()}`);
+
+console.log(names.split(','));
