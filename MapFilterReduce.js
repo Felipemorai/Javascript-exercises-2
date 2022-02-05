3c037837efee041ac05c320e9fb10abc416c3143
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-05 15:49:48 -0300

    map

---
+++ MapFilterReduce.js
@@ -0,0 +1,8 @@
+/* Method Map() */
+const numbers = [1, 2, 3, 4, 5];
+
+const numbersMultipliedByToo = numbers.map(function(number) {
+    return number * 2
+});
+
+console.log(numbersMultipliedByToo);
