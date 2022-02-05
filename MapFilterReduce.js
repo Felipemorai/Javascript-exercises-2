2c9618c286a35336f3028fb0d7e755a17b5c91b8
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-05 16:00:36 -0300

    filter

--- MapFilterReduce.js
+++ MapFilterReduce.js
@@ -6,3 +6,12 @@ const numbersMultipliedByToo = numbers.map(function(number) {
 });
 
 console.log(numbersMultipliedByToo);
+
+/* Method Filter */
+const ages = [8, 13, 27, 30, 22, 7, 40];
+
+const evenAges = ages.filter(function(age) {
+    return age % 2 === 0;
+});
+
+console.log(evenAges);
