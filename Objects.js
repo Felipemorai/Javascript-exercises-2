547d0f91a43ffb3a63e7e5334b1f285731d1ba05
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-07 22:28:00 -0300

    object

---
+++ Objects.js
@@ -0,0 +1,16 @@
+const person = {
+    firstName: 'Don',
+    lastName: 'Corleone',
+    age: '23',
+    job: 'Gangsta'
+};
+
+const firstName = person.firstName;
+const lastName = person.lastName;
+const age = person.age;
+const job = person.job;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(job);
