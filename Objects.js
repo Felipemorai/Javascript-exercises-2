23324c9a88f06247f168934f85c94aca09bf2956
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-08 22:13:13 -0300

    destructing

--- Objects.js
+++ Objects.js
@@ -2,15 +2,31 @@ const person = {
     firstName: 'Don',
     lastName: 'Corleone',
     age: '23',
-    job: 'Gangsta'
+    job: 'Gangsta',
+    hobbies: ['To watch football', 'To play football', 'Heist'],
+    dog: {
+        name: 'Twig',
+        age: 4
+    }
 };
 
-const firstName = person.firstName;
+/* const firstName = person.firstName;
 const lastName = person.lastName;
 const age = person.age;
 const job = person.job;
+ */
+const heist = person.hobbies[2];
 
-console.log(firstName);
+/* Destructing */
+const { firstName: primeiroNome, lastName, age, job, hobbies, dog: { name }, } = person;
+
+console.log(primeiroNome);
 console.log(lastName);
 console.log(age);
 console.log(job);
+console.log(heist);
+
+/* person.dog = 'Twig'; */
+
+console.log(person.dog.name);
+console.log(name);
