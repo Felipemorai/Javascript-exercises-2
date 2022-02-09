179cfa3c44ebc7c69941b4fa9fb0918e7b3080b4
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-09 15:33:09 -0300

    Loops

---
+++ Loops.js
@@ -0,0 +1,10 @@
+/* For Loop */
+for (let index = 0; index <= 10; index++) {
+    console.log(index);
+}; 
+
+const carsBrand = ['ferrari', 'Tesla', 'Ford', 'Mercedes'];
+
+for (let i = 0; i < carsBrand.length; i++) {
+    console.log(carsBrand[i]);
+};
