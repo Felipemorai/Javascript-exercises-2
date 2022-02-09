107dfc26af907e738e8b254059f5730702e6def9
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-09 14:33:03 -0300

    json

---
+++ Json.js
@@ -0,0 +1,21 @@
+const all = [
+    {
+        id: 1,
+        description: 'Study programmation',
+        isCompleted: false,
+    },
+    {
+        id: 2,
+        description: 'Read',
+        isCompleted: true,
+    },
+    {
+        id: 3,
+        description: 'Train',
+        isCompleted: true,
+    },
+];
+
+const descriptionOfLastAll = all[2].description;
+
+console.log(descriptionOfLastAll);
