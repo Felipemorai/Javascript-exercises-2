295d2dec0d0b5260b4585c3fcc61465ffca97f23
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-10 14:13:06 -0300

    while

--- Loops.js
+++ Loops.js
@@ -8,3 +8,19 @@ const carsBrand = ['ferrari', 'Tesla', 'Ford', 'Mercedes'];
 for (let i = 0; i < carsBrand.length; i++) {
     console.log(carsBrand[i]);
 };
+
+/* For Of Loop */
+for (let car of carsBrand) {
+    console.log(car);
+};
+
+carsBrand.forEach(function(car, index) {
+    console.log(index, car);
+});
+
+/* While Loop */
+let index = 0;
+
+while (index < 10) {
+    console.log('index it´s smaller than 10')
+}
