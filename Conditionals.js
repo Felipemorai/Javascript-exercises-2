73b2d0213d31a3740db714b277a1b265c67006ce
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-12 13:16:08 -0300

    If and Else

---
+++ Conditionals.js
@@ -0,0 +1,27 @@
+/* Conditionals If and Else */
+const sum = 2 + 2;
+
+if (sum === 2) {
+    console.log('Sum is 2!');
+} else if (sum ===3) {
+    console.log('Sum is 3!');
+}
+else {
+    console.log('Sum isn´t 2!');
+}
+
+/* Operators And(&&) and Or(||) */
+const sum1 = 2 + 2;
+const sum2 = 3 + 3;
+
+if (sum1 === 4 && sum2 === 6) {
+    console.log('sum1 is 4 and sum2 is 6!');
+} else {
+    console.log('sum1 isn´t 4 and sum2 isn´t 6!');
+}
+
+if (sum1 === 4 || sum2 === 6) {
+    console.log('sum1 is 4 or sum2 is 6!');
+} else {
+    console.log('sum1 isn´t 4 or sum2 isn´t 6');
+}
