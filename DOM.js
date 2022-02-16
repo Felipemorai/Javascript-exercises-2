53e6c96d4a9f801938ba67bc8d897886fc2f8133
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-16 15:58:45 -0300

    selecting dom

---
+++ DOM.js
@@ -0,0 +1,11 @@
+const addUserText = document.getElementById('add-user');
+
+addUserText.innerText = "Adicionar usuário";
+
+console.log(addUserText);
+
+const submitButtonText = document.querySelector('#submit-button');
+
+submitButtonText.value = "Criar";
+
+console.log(submitButtonText);
