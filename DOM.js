8f0c4333cd37c767161d03b8ee1960562aaa6a8d
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-16 16:19:22 -0300

    selector

--- DOM.js
+++ DOM.js
@@ -9,3 +9,23 @@ const submitButtonText = document.querySelector('#submit-button');
 submitButtonText.value = "Criar";
 
 console.log(submitButtonText);
+
+const item = document.querySelector(".item");
+
+console.log(item);
+
+/* Select multiple elements */
+const allItems = document.querySelectorAll(".item");
+
+console.log(allItems);
+console.log(allItems[1]); /* Select item of the middle */
+
+/* getElementsByClassName isn't good, you can't to do methods */
+const allItem = document.getElementsByClassName("item");
+
+console.log(allItem);
+
+/* getElementsByTagName */
+const allItemss = document.getElementsByTagName("li");
+
+console.log(allItemss);
