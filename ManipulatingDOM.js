d2315e28319f9f262e52e8a1caad323ad7e25e61
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-17 16:42:03 -0300

    styling

--- ManipulatingDOM.js
+++ ManipulatingDOM.js
@@ -1,5 +1,6 @@
 /* Manipulate */
 const items = document.querySelector('.items');
+const button = document.querySelector('.btn');
 
 /* items.remove(); Remove everything*/
 
@@ -7,5 +8,9 @@ items.firstElementChild.remove();
 
 items.lastElementChild.remove();
 
-items.remove([1]);
+/* items.remove([1]); */
 
+items.children[0].textContent = 'Item One';
+
+/* Styling button */
+button.style.background = "red";
