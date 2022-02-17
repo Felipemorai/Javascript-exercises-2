14e2a31baec47241a00166398ba8b13d13a5d6a1
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-17 09:22:25 -0300

    remove

---
+++ ManipulatingDOM.js
@@ -0,0 +1,11 @@
+/* Manipulate */
+const items = document.querySelector('.items');
+
+/* items.remove(); Remove everything*/
+
+items.firstElementChild.remove();
+
+items.lastElementChild.remove();
+
+items.remove([1]);
+
