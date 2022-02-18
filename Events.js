44eb38e971edadcc354ecad8dfde82436c304e00
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-02-18 09:03:09 -0300

    Events

--- Events.js
+++ Events.js
@@ -1,6 +1,8 @@
 const submitButton = document.querySelector("#submit-button");
+const myForm = document.querySelector("#my-form");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
+const items = document.querySelector(".items");
 
 submitButton.addEventListener("click", function(e) {
     e.preventDefault();
@@ -12,4 +14,16 @@ submitButton.addEventListener("click", function(e) {
 
     const emailValue = emailInput.value;
     console.log(emailValue);
+
+    if (nameValue === '' || emailValue === '') {
+        return alert('Please fill out all the fields!');
+    }
+
+    myForm.style.background = "red";
+    items.firstElementChild.textContent = nameValue;
+    items.children[1].textContent = emailValue;
+});
+
+nameInput.addEventListener('change', function(e) {
+    console.log(e.target.value);
 });
